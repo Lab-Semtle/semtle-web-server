bc2a72794717320c1e61f3991f8f01cd238d46b5
e-wha <ghj4775@naver.com>
2024-08-26 21:39:37 +0900

    Update __init__.py

---
+++ src/api/v1/__init__.py
@@ -0,0 +1,21 @@
+from fastapi import APIRouter
+from src.api.v1.exam_sharing_board.exam_sharing_board_ctl import router as exam_sharing_board_router
+from src.api.v1.exam_sharing_board_comment.exam_sharing_board_comment_ctl import router as exam_sharing_board_comment_router
+from src.api.v1.free_board.free_board_ctl import router as free_board_router
+from src.api.v1.free_board_comment.free_board_comment_ctl import router as free_board_comment_router
+from src.api.v1.login.login_control import router as login_router
+from src.api.v1.study_board.study_board_ctl import router as study_board_router
+from src.api.v1.study_board_comment.study_board_comment_ctl import router as study_board_comment_router
+from src.api.v1.user.user_control import router as user_router
+
+
+
+router = APIRouter()
+router.include_router(exam_sharing_board_router)
+router.include_router(exam_sharing_board_comment_router)
+router.include_router(free_board_router)
+router.include_router(free_board_comment_router)
+router.include_router(login_router)
+router.include_router(study_board_router)
+router.include_router(study_board_comment_router)
+router.include_router(user_router)
