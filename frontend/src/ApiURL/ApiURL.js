139282e449d564fb1b4fb9f3b804d8ffe55e8093
ormor <htth815@gmail.com>
2024-08-31 02:36:14 +0900

    FIX: 병합중 오류발생 이전 병합이전으로 복귀

--- frontend/src/ApiURL/ApiURL.js
+++
@@ -1,6 +0,0 @@
-const ApiURL={
-    Boardlist_get:`https://gist.githubusercontent.com/minseozzing/976893f1a782e503607a12db93184ef8/raw/c8d76e66935c763b6ecc0346cdb500babe3a0752/gsdfgergewxx.json`,
-    Boardview_get:`https://gist.githubusercontent.com/minseozzing/2036f94525929d9ac74b508dd036b263/raw/9ed7fe688e397bb76173983f5c9a16c219f73960/gistfiwefwef.json`,
-    Boardcreate_post:`https://reqres.in/api/users/2`
-}
-export{ApiURL}
