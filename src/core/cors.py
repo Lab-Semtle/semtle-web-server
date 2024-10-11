e68c24c607e8142c99166570af53e8bcd39eea88
ormor <htth815@gmail.com>
2024-10-11 22:52:38 +0900

    🛠️  CORS origin * -> localhost:3000

--- src/core/cors.py
+++ src/core/cors.py
@@ -9,7 +9,7 @@ def use(app: FastAPI):
     """ FastAPI 애플리케이션에 CORS 미들웨어를 설정 """
     app.add_middleware(
         CORSMiddleware,
-        allow_origins=["*"],    # 모든 도메인 요청 허용
+        allow_origins=["http://localhost:3000"],    # 모든 도메인 요청 허용
         allow_methods=["*"],    # 모든 메서드 요청 허용
         allow_headers=["*"],    # 요청 시 모든 헤더 허용
         expose_headers=["*"],   # 모든 헤더 자바스크립트 접근 허용
